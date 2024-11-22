73e7c2c28bfb76fe0d88e120441aa9a5f7e8e7cf
Hauh4444 <150862142+Hauh4444@users.noreply.github.com>
2024-11-22 15:32:43 -0500

    View Pothole

--- src/ptrs/app/__init__.py
+++ src/ptrs/app/__init__.py
@@ -1,15 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-'''
+"""
 When you import the ptrs.app package,
 all code in this module will be run automatically.
-'''
+"""
+
+NEWPOTHOLEINFO = {
+    "address": "",
+    "lattitude": 0,
+    "longitude": 0,
+    "size": 0,
+    "location": "",
+    "other": "",
+}
+
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
-    @app.route('/about')
+    @app.route("/about")
     def about():
-        return 'Pothole Tracking and Repair System (PTRS)'
-    
+        return "Pothole Tracking and Repair System (PTRS)"
+
+    @app.route("/pothole", methods=["GET", "POST"])
+    def pothole():
+        if request.method == "POST":
+            NEWPOTHOLEINFO["size"] = float(request.form.get("size")) / 10
+            NEWPOTHOLEINFO["location"] = request.form.get("location")
+            NEWPOTHOLEINFO["other"] = request.form.get("other")
+        return render_template("pothole.html")
+
+    @app.route("/addressUpdate", methods=["POST"])
+    def process_data():
+        NEWPOTHOLEINFO["address"] = request.json["address"]
+        NEWPOTHOLEINFO["lattitude"] = request.json["lattitude"]
+        NEWPOTHOLEINFO["longitude"] = request.json["longitude"]
+        return "success"
+
     return app
