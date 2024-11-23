93cca023f259cc34eac0809eae2066b8efffa222
Hauh4444 <150862142+Hauh4444@users.noreply.github.com>
2024-11-23 16:04:55 -0500

    update

--- src/ptrs/static/map.js
+++ src/ptrs/static/map.js
@@ -1,40 +1,45 @@
-/*map.on("click", function (e) {
-	[markerGPS.longitude, markerGPS.latitude] = [parseFloat(e.lngLat.lng), parseFloat(e.lngLat.lat)];
-	mapMarker.setLngLat([markerGPS.longitude, markerGPS.latitude]).addTo(map);
-	let url = `https://api.geoapify.com/v1/geocode/reverse?lat=${markerGPS.latitude}&lon=${markerGPS.longitude}&apiKey=cae0e983bd184089b8f89641f5538ee8`;
-
-	fetch(url)
-		.then((result) => result.json())
-		.then((featureCollection) => {
-			markerGPS.address = featureCollection.features[0].properties.formatted.replace(", United States of America", "");
-			document.querySelector("#address").innerHTML = markerGPS.address;
-
-			fetch("http://127.0.0.1:5000/data", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ address: markerGPS.address, latitude: markerGPS.latitude, longitude: markerGPS.longitude }),
-			})
-				.then((response) => response.text())
-				.catch((error) => {
-					console.error("Error:", error);
-				});
-		});
-});*/
-
-
 async function initMap() {
 	const { Map } = await google.maps.importLibrary("maps");
-	const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-		"marker",
-	);
-	const myLatLng = {lng: -79.1525, lat: 40.6215};
+	const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
+	const myLatLng = { lng: -79.1525, lat: 40.6215 };
 	const map = new Map(document.getElementById("map"), {
 		center: myLatLng,
 		zoom: 12,
 		mapId: "c894f5bb0ee453ef",
-	})
+	});
+	const marker = new AdvancedMarkerElement({
+		map,
+		position: myLatLng,
+		title: 'Uluru',
+	});
+
+	map.addListener("click", (e) => {
+		let latitude = e.latLng.lat();
+		let longitude = e.latLng.lng();
+		marker.position = { lat: latitude, lng: longitude };
+
+		let url = `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=cae0e983bd184089b8f89641f5538ee8`;
+
+		fetch(url)
+			.then((result) => result.json())
+			.then((featureCollection) => {
+				let address = featureCollection.features[0].properties.formatted.replace(", United States of America", "");
+				document.querySelector("#address").innerHTML = address;
+
+				fetch("http://127.0.0.1:5000/data", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ address: address, latitude: latitude, longitude: longitude }),
+				})
+					.then((response) => response.text())
+					.catch((error) => {
+						console.error("Error:", error);
+					});
+			});
+	});
+
 	fetch("http://127.0.0.1:5000/data", {
 		method: "GET",
 		headers: {
