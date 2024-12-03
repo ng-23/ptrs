aebf6f3a1f53c4e829ca56acae7dfff538689529
Hauh4444 <150862142+Hauh4444@users.noreply.github.com>
2024-12-02 19:43:24 -0500

    update

--- src/ptrs/app/__init__.py
+++ src/ptrs/app/__init__.py
@@ -1,5 +1,6 @@
 import os
-from ptrs.app import database
+from ptrs.app import controllers, database
+from ptrs.app.model import services
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
@@ -12,9 +13,34 @@ all code in this module will be run automatically.
 load_dotenv()
 
 
+def add_routable_controllers(app: Flask):
+    """
+    A routable Controller is a Controller with an API route assigned to it
+    These Controllers are intended to be interacted with by the user via HTTP request methods
+    To allow Flask to route requests to these Controllers, they must be added to the app instance
+    """
+    for (url_rule, req_method), controller_vars in controllers.routable_controllers.items():
+        data_mappers = [
+            data_mapper_class()
+            for data_mapper_class in services.registered_services[controller_vars["service_class"]]["data_mappers"]
+        ]  # instantiate DataMappers the Controller's Service depends on
+        service = controller_vars["service_class"](*data_mappers)  # instantiate the Service the Controller and its View depend on
+        view = controller_vars["view_class"](service)  # instantiate the View the Controller depends on
+        service.register_observer(view)  # View now observes the Service, so it will be notified by the Service when the Service changes the Model's state
+
+        # see https://stackoverflow.com/questions/19261833/what-is-an-endpoint-in-flask
+        # to understand how Flask defines and manages routes
+        app.add_url_rule(
+            url_rule,
+            endpoint=controller_vars["endpoint"],
+            view_func=controller_vars["controller_class"].as_view(controller_vars["endpoint"], service, view)
+        )
+
+
 def create_app():
     app = Flask(__name__)
     app.config["DATABASE"] = os.getenv("DATABASE")
+    add_routable_controllers(app)
 
     with app.app_context():
         database.init_db(
